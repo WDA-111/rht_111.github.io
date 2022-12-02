e08e91cdcd4e1518f7631b47caaf4ee578531dd4
H4CKER <109261767+WDA-111@users.noreply.github.com>
2022-12-02 10:27:14 +0600

    this is the first upload
    this is first upload so it is not complite site i will reupload updats

---
+++ script.js
@@ -0,0 +1,9 @@
+const hambarger = document.querySelector(".hambarger");
+const navi = document.querySelector(".navi");
+
+hambarger.addEventListener("click",() =>{
+    hambarger.classList.toggle("active");
+    navi.classList.toggle("active");
+})
+
+console.log("WELCOME TO RHT 111");
